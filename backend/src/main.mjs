122e8bb56aa53be450df4e34f5f02dcac0bef7fb
Logan Moore <lmooore@gmail.com>
2025-03-29 14:16:38 -0500

    get index.html & client script to load

--- backend/src/main.mjs
+++ backend/src/main.mjs
@@ -3,15 +3,20 @@ import * as path from "jsr:@std/path";
 Deno.serve(async req => {
   const url = new URL(req.url);
   const { pathname } = url;
+  const { signal } = req;
 
   if (pathname === '/' || !pathname) {
     // serve the index.html file
-    return new Response(await Deno.readTextFile("./index.html", { signal }));
+    return new Response(await Deno.readTextFile("./index.html", { signal }), {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
   }
 
   const localPath = path.join('.', pathname);
 
-  if (pathname.startsWith('/backend/src/')) {
+  if (pathname.startsWith('/frontend/src/')) {
     return new Response(await Deno.readTextFile(localPath, { signal }));
   }
 
